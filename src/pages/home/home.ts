3641cbcd5c393d09f599c1d0fb7dbfb35d0642b5
Arek Ruta <arek.ruta89@gmail.com>
2017-07-09 16:26:44 +0200

    add map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(50.262951, 19.006107);
+    let mapOptions = {
+      center: latLng,
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
 }
